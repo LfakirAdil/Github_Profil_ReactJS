74a2872101dfd4a1de5c52941678711be77fbe58
LFAKIR Adil <39201375+LfakirAdil@users.noreply.github.com>
2022-01-26 21:46:51 +0100

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Profil Github</h1>
+      
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+     
+    </>
+  )
+}
+
+export default About
